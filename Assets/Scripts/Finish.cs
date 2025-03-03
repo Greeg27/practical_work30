81d64335221987072e2216082dc5752367992e1a
Григорий Тарнавский <gabuska@inbox.ru>
2025-03-03 23:12:56 +0300

    ui editing

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class Finish : MonoBehaviour
 {
     [SerializeField] GameObject levelComplete;
+    [SerializeField] GameObject timer;
+    [SerializeField] GameObject pause;
 
     private void OnTriggerEnter(Collider other)
     {
         Time.timeScale = 0;
+
+        timer.SetActive(false);
+        pause.SetActive(false);
         levelComplete.SetActive(true);
     }
 }
